7b93a3f40485e496cf612a53583c6b90d4c6dd15
ATSU <mifutsua@gmail.com>
2021-02-24 15:43:23 +0900

    get room temperature

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const Env = require('dotenv').config();
 var obniz = new Obniz(process.env.PASSWORD);
 obniz.onconnect = async function () {
   obniz.display.clear();
-  obniz.display.print("Hello!");
+  var tempsens = obniz.wired("LM35DZ", { gnd: 7, output: 8, vcc: 9 });
+  var temp = await tempsens.getWait();
+  console.log(temp);
+  obniz.display.print(temp);
 }
