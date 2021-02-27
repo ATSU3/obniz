666cd42a7428c0b9da6dfc20d8c2a28147e91e01
ATSU <mifutsua@gmail.com>
2021-02-27 11:55:22 +0900

    control the air conditioner

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const Obniz = require("obniz");
 const Env = require('dotenv').config();
+const fs = require('fs');
+const jsonObject = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+const result = {};
+var masterData = [];
+var express = require("express");
+var app = express();
 
 var obniz = new Obniz(process.env.PASSWORD);
 obniz.onconnect = async function () {
@@ -8,4 +14,13 @@ obniz.onconnect = async function () {
   var temp = await tempsens.getWait();
   console.log(temp);
   obniz.display.print(temp);
+  var module = obniz.wired('IRModule', { vcc: 0, send: 1, recv: 2, gnd: 3 });
+
+  jsonObject.Data.forEach((obj) => {
+    result[obj.Data] = obj;
+    console.log(obj.turnOn, obj.turnOff)
+    module.send(obj.turnOn)
+    module.duration = 10000;
+    module.send(obj.turnOff)
+  });
 }
